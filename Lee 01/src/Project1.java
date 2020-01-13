60cee9b76cde80ece786f564b436b0c1a09c88f7
Joseph Lee <jtlbo.DESKTOP-59htpku>
2020-01-12 19:43:17 -0500

    My First Commit

---
+++ Lee 01/src/Project1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;;
+
+public class Project1 {
+	public static void main(String[] args) {
+		// TODO Auto generated stub
+		Scanner jtf = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = jtf.nextLine();
+		
+		System.out.println("Hello " + name);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
